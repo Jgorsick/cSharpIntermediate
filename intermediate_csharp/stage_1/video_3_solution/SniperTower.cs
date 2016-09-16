45157e19d29952abc1c17728d4592f7f4683b37c
Joe Gorsick <jgorsick@gmail.com>
2016-09-16 17:27:10 -0400

    Expanding game capabilities utilizing properties created with "virtual" and "override" keywords and restricting there accessability with the 'protected" keword.

---
+++ intermediate_csharp/stage_1/video_3_solution/SniperTower.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TreehouseDefense
+{
+    class SniperTower : Tower
+    {
+        protected override int Range { get; } = 2;
+        protected double Accuracy { get; } = 1.0;
+	}
+}
